f69181224f70c54ebe830b059639927cb090e45f
Serpensin <wissmann.matti@gmail.com>
2023-06-20 02:18:19 +0200

    added topgg counter

--- WebhookCreator/main.py
+++ WebhookCreator/main.py
@@ -1,4 +1,5 @@
 #Import
+import asyncio
 import discord
 import json
 import jsonschema
@@ -7,235 +8,264 @@ import platform
 import sentry_sdk
 import sys
 import time
-from dotenv import load_dotenv
+import topgg
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
 
 
 #Sentry
 discord.VoiceClient.warn_nacl = False
 load_dotenv()
 sentry_sdk.init(
-    dsn=os.getenv('SENTRY_DSN'),
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
-    environment='Production',
+	dsn=os.getenv('SENTRY_DSN'),
+	traces_sample_rate=1.0,
+	profiles_sample_rate=1.0,
+	environment='Production',
 )
 
 app_folder_name = 'WH-Creator'
 bot_name = 'WebhookCreator'
 if not os.path.exists(app_folder_name):
-    os.makedirs(app_folder_name)
+	os.makedirs(app_folder_name)
 activity_file = os.path.join(app_folder_name, 'activity.json')
 bot_version = "1.2.0"
 TOKEN = os.getenv('TOKEN')
 ownerID = os.getenv('OWNER_ID')
 support_id = os.getenv('SUPPORT_SERVER')
+topgg_token = os.getenv('TOPGG_TOKEN')
+
 
 #Create activity.json if not exists
 class JSONValidator:
-    schema = {
-        "type" : "object",
-        "properties" : {
-            "activity_type" : {
-                "type" : "string", 
-                "enum" : ["Playing", "Streaming", "Listening", "Watching", "Competing"]
-            },
-            "activity_title" : {"type" : "string"},
-            "activity_url" : {"type" : "string"},
-            "status" : {
-                "type" : "string",
-                "enum" : ["online", "idle", "dnd", "invisible"]
-            },
-        },
-    }
+	schema = {
+		"type" : "object",
+		"properties" : {
+			"activity_type" : {
+				"type" : "string", 
+				"enum" : ["Playing", "Streaming", "Listening", "Watching", "Competing"]
+			},
+			"activity_title" : {"type" : "string"},
+			"activity_url" : {"type" : "string"},
+			"status" : {
+				"type" : "string",
+				"enum" : ["online", "idle", "dnd", "invisible"]
+			},
+		},
+	}
 
-    default_content = {
-        "activity_type": "Playing",
-        "activity_title": "Made by Serpensin: https://gitlab.bloodygang.com/Serpensin",
-        "activity_url": "",
-        "status": "online"
-    }
+	default_content = {
+		"activity_type": "Playing",
+		"activity_title": "Made by Serpensin: https://gitlab.bloodygang.com/Serpensin",
+		"activity_url": "",
+		"status": "online"
+	}
 
-    def __init__(self, file_path):
-        self.file_path = file_path
+	def __init__(self, file_path):
+		self.file_path = file_path
 
-    def validate_and_fix_json(self):
-        if os.path.exists(self.file_path):
-            with open(self.file_path, 'r') as file:
-                try:
-                    data = json.load(file)
-                    jsonschema.validate(instance=data, schema=self.schema)  # validate the data
-                except jsonschema.exceptions.ValidationError as ve:
-                    print(f'ValidationError: {ve}')
-                    self.write_default_content()
-                except json.decoder.JSONDecodeError as jde:
-                    print(f'JSONDecodeError: {jde}')
-                    self.write_default_content()
-        else:
-            self.write_default_content()
+	def validate_and_fix_json(self):
+		if os.path.exists(self.file_path):
+			with open(self.file_path, 'r') as file:
+				try:
+					data = json.load(file)
+					jsonschema.validate(instance=data, schema=self.schema)  # validate the data
+				except jsonschema.exceptions.ValidationError as ve:
+					print(f'ValidationError: {ve}')
+					self.write_default_content()
+				except json.decoder.JSONDecodeError as jde:
+					print(f'JSONDecodeError: {jde}')
+					self.write_default_content()
+		else:
+			self.write_default_content()
 
-    def write_default_content(self):
-        with open(self.file_path, 'w') as file:
-            json.dump(self.default_content, file, indent=4)
+	def write_default_content(self):
+		with open(self.file_path, 'w') as file:
+			json.dump(self.default_content, file, indent=4)
 validator = JSONValidator(activity_file)
 validator.validate_and_fix_json()
 
 
 class aclient(discord.AutoShardedClient):
-    def __init__(self):
+	def __init__(self):
 
-        intents = discord.Intents.default()
+		intents = discord.Intents.default()
 
-        super().__init__(owner_id = ownerID,
-                              intents = intents,
-                              status = discord.Status.invisible
-                        )
-        self.synced = False
-    async def on_ready(self):
-        if not self.synced:
-            await tree.sync()
-            self.synced = True
-            await bot.change_presence(activity = discord.Game(name='with Webhooks'), status = discord.Status.online)
-        global owner, start_time
-        owner = await bot.fetch_user(ownerID)
-        start_time = datetime.now()
-        print('READY')
+		super().__init__(owner_id = ownerID,
+							  intents = intents,
+							  status = discord.Status.invisible
+						)
+		self.synced = False
+	async def on_ready(self):
+		if not self.synced:
+			await tree.sync()
+			self.synced = True
+			await bot.change_presence(activity = discord.Game(name='with Webhooks'), status = discord.Status.online)
+		
+		if topgg_token:
+			bot.topggpy = topgg.DBLClient(bot, topgg_token)
+			bot.loop.create_task(Functions.update_topgg())
+
+		global owner, start_time, shutdown
+		shutdown = False
+		owner = await bot.fetch_user(ownerID)
+		start_time = datetime.now()
+		print('READY')
 bot = aclient()
 tree = discord.app_commands.CommandTree(bot)
 
 
 # Check if all required variables are set
-owner_available = bool(ownerID)
 support_available = bool(support_id)
+
+
 
 
 ##Events
 #Error
 @tree.error
 async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError) -> None:
-    await interaction.response.send_message(error, ephemeral = True)
+	await interaction.response.send_message(error, ephemeral = True)
 
  
 #Functions
 class Functions():
-    async def create_support_invite(interaction):
-        try:
-            guild = bot.get_guild(int(support_id))
-        except ValueError:
-            return "Could not find support guild."
-        if guild is None:
-            return "Could not find support guild."
-        if not guild.text_channels:
-            return "Support guild has no text channels."
-        try:
-            member = await guild.fetch_member(interaction.user.id)
-        except discord.NotFound:
-            member = None
-        if member is not None:
-            return "You are already in the support guild."
-        channels: discord.TextChannel = guild.text_channels
-        for channel in channels:
-            try:
-                invite: discord.Invite = await channel.create_invite(
-                    reason=f"Created invite for {interaction.user.name} from server {interaction.guild.name} ({interaction.guild_id})",
-                    max_age=60,
-                    max_uses=1,
-                    unique=True
-                )
-                return invite.url
-            except discord.Forbidden:
-                continue
-            except discord.HTTPException:
-                continue
-        return "Could not create invite. There is either no text-channel, or I don't have the rights to create an invite."
+	async def create_support_invite(interaction):
+		try:
+			guild = bot.get_guild(int(support_id))
+		except ValueError:
+			return "Could not find support guild."
+		if guild is None:
+			return "Could not find support guild."
+		if not guild.text_channels:
+			return "Support guild has no text channels."
+		try:
+			member = await guild.fetch_member(interaction.user.id)
+		except discord.NotFound:
+			member = None
+		if member is not None:
+			return "You are already in the support guild."
+		channels: discord.TextChannel = guild.text_channels
+		for channel in channels:
+			try:
+				invite: discord.Invite = await channel.create_invite(
+					reason=f"Created invite for {interaction.user.name} from server {interaction.guild.name} ({interaction.guild_id})",
+					max_age=60,
+					max_uses=1,
+					unique=True
+				)
+				return invite.url
+			except discord.Forbidden:
+				continue
+			except discord.HTTPException:
+				continue
+		return "Could not create invite. There is either no text-channel, or I don't have the rights to create an invite."
+
+
+	async def update_topgg():
+		while not shutdown:
+			await bot.topggpy.post_guild_count()
+			try:
+				await asyncio.sleep(60*30)
+			except asyncio.CancelledError:
+				pass
+
 
 
 ##Owner Commands----------------------------------------
 #Shutdown
-if owner_available:
-    @tree.command(name = 'shutdown', description = 'Safely shut down the bot.')
-    async def self(interaction: discord.Interaction):
-        if interaction.user.id == int(ownerID):
-            await interaction.response.send_message('Engine powering down...', ephemeral = True)
-            await bot.close()
-        else:
-            await interaction.response.send_message('Only the BotOwner can use this command!', ephemeral = True)
+@tree.command(name = 'shutdown', description = 'Safely shut down the bot.')
+async def self(interaction: discord.Interaction):
+	if interaction.user.id == int(ownerID):
+		global shutdown
+		shutdown = True
+		await interaction.response.send_message('Engine powering down...', ephemeral = True)
+
+		tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+		[task.cancel() for task in tasks]
+		await asyncio.gather(*tasks, return_exceptions=True)
+
+		await bot.close()
+	else:
+		await interaction.response.send_message('Only the BotOwner can use this command!', ephemeral = True)
 ##Bot Commands----------------------------------------
 #Bot Information
 @tree.command(name = 'botinfo', description = 'Get information about the bot.')
 @discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.user.id))
 async def self(interaction: discord.Interaction):
-    member_count = sum(guild.member_count for guild in bot.guilds)
+	member_count = sum(guild.member_count for guild in bot.guilds)
 
-    embed = discord.Embed(
-        title=f"Informationen about {bot.user.name}",
-        color=discord.Color.blue()
-    )
-    embed.set_thumbnail(url=bot.user.avatar.url if bot.user.avatar else '')
+	embed = discord.Embed(
+		title=f"Informationen about {bot.user.name}",
+		color=discord.Color.blue()
+	)
+	embed.set_thumbnail(url=bot.user.avatar.url if bot.user.avatar else '')
 
-    embed.add_field(name="Created at", value=bot.user.created_at.strftime("%d.%m.%Y, %H:%M:%S"), inline=True)
-    embed.add_field(name="Bot-Version", value=bot_version, inline=True)
-    embed.add_field(name="Uptime", value=str(timedelta(seconds=int((datetime.now() - start_time).total_seconds()))), inline=True)
+	embed.add_field(name="Created at", value=bot.user.created_at.strftime("%d.%m.%Y, %H:%M:%S"), inline=True)
+	embed.add_field(name="Bot-Version", value=bot_version, inline=True)
+	embed.add_field(name="Uptime", value=str(timedelta(seconds=int((datetime.now() - start_time).total_seconds()))), inline=True)
 
-    embed.add_field(name="Bot-Owner", value=f"<@!{ownerID}>", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
+	embed.add_field(name="Bot-Owner", value=f"<@!{ownerID}>", inline=True)
+	embed.add_field(name="\u200b", value="\u200b", inline=True)
+	embed.add_field(name="\u200b", value="\u200b", inline=True)
 
-    embed.add_field(name="Server", value=f"{len(bot.guilds)}", inline=True)
-    embed.add_field(name="Member count", value=str(member_count), inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
+	embed.add_field(name="Server", value=f"{len(bot.guilds)}", inline=True)
+	embed.add_field(name="Member count", value=str(member_count), inline=True)
+	embed.add_field(name="\u200b", value="\u200b", inline=True)
 
-    embed.add_field(name="Shards", value=f"{bot.shard_count}", inline=True)
-    embed.add_field(name="Shard ID", value=f"{interaction.guild.shard_id if interaction.guild else 'N/A'}", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
+	embed.add_field(name="Shards", value=f"{bot.shard_count}", inline=True)
+	embed.add_field(name="Shard ID", value=f"{interaction.guild.shard_id if interaction.guild else 'N/A'}", inline=True)
+	embed.add_field(name="\u200b", value="\u200b", inline=True)
 
-    embed.add_field(name="Python-Version", value=f"{platform.python_version()}", inline=True)
-    embed.add_field(name="discord.py-Version", value=f"{discord.__version__}", inline=True)
-    embed.add_field(name="Sentry-Version", value=f"{sentry_sdk.consts.VERSION}", inline=True)
+	embed.add_field(name="Python-Version", value=f"{platform.python_version()}", inline=True)
+	embed.add_field(name="discord.py-Version", value=f"{discord.__version__}", inline=True)
+	embed.add_field(name="Sentry-Version", value=f"{sentry_sdk.consts.VERSION}", inline=True)
 
-    embed.add_field(name="Repo", value=f"[GitLab](https://gitlab.bloodygang.com/Serpensin/Discord-Webhook-Creator)", inline=True)
-    embed.add_field(name="Invite", value=f"[Invite me](https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=536870912&scope=bot%20applications.commands)", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)  
+	embed.add_field(name="Repo", value=f"[GitLab](https://gitlab.bloodygang.com/Serpensin/Discord-Webhook-Creator)", inline=True)
+	embed.add_field(name="Invite", value=f"[Invite me](https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=536870912&scope=bot%20applications.commands)", inline=True)
+	embed.add_field(name="\u200b", value="\u200b", inline=True)  
 
-    await interaction.response.send_message(embed=embed)
+	await interaction.response.send_message(embed=embed)
 #Support Invite
 if support_available:
-    @tree.command(name = 'support', description = 'Get invite to our support server.')
-    @discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.user.id))
-    async def self(interaction: discord.Interaction):
-        if str(interaction.guild.id) != support_id:
-            await interaction.response.defer(ephemeral = True)
-            await interaction.followup.send(await Functions.create_support_invite(interaction), ephemeral = True)
-        else:
-            await interaction.response.send_message('You are already in our support server!', ephemeral = True)
+	@tree.command(name = 'support', description = 'Get invite to our support server.')
+	@discord.app_commands.checks.cooldown(1, 60, key=lambda i: (i.user.id))
+	async def self(interaction: discord.Interaction):
+		if str(interaction.guild.id) != support_id:
+			await interaction.response.defer(ephemeral = True)
+			await interaction.followup.send(await Functions.create_support_invite(interaction), ephemeral = True)
+		else:
+			await interaction.response.send_message('You are already in our support server!', ephemeral = True)
 #Ping
 @tree.command(name = 'ping', description = 'Test, if the bot is responding.')
 async def self(interaction: discord.Interaction):
-    before = time.monotonic()
-    await interaction.response.send_message('Pong!')
-    ping = (time.monotonic() - before) * 1000
-    await interaction.edit_original_response(content=f'Pong! `{int(ping)}ms`')
+	before = time.monotonic()
+	await interaction.response.send_message('Pong!')
+	ping = (time.monotonic() - before) * 1000
+	await interaction.edit_original_response(content=f'Pong! `{int(ping)}ms`')
 ##Main Commands----------------------------------------
 #Create Webhook
 @tree.command(name = 'create_webhook', description = 'Create a webhook.')
 @discord.app_commands.describe(name='Name of the webhook.')
 async def self(interaction: discord.Interaction, name: str):
-    if interaction.channel.permissions_for(interaction.user).manage_webhooks:
-        webhook = await interaction.channel.create_webhook(name=name, reason=f'Created by {interaction.user.name}#{interaction.user.discriminator} ({interaction.user.id})')
-        await interaction.response.send_message(webhook.url, ephemeral=True)
-    else:
-        await interaction.response.send_message('You need the permission "Manage Webhooks" for this channel to use this command!', ephemeral=True)
+	if interaction.channel.permissions_for(interaction.user).manage_webhooks:
+		webhook = await interaction.channel.create_webhook(name=name, reason=f'Created by {interaction.user.name}#{interaction.user.discriminator} ({interaction.user.id})')
+		await interaction.response.send_message(webhook.url, ephemeral=True)
+	else:
+		await interaction.response.send_message('You need the permission "Manage Webhooks" for this channel to use this command!', ephemeral=True)
 
 
 
 
 
 if __name__ == '__main__':
-    if not TOKEN:
-        sys.exit('Missing token. Please check your .env file.')
-    else:
-        try:
-            bot.run(TOKEN, log_handler=None)
-        except discord.errors.LoginFailure:
-            sys.exit('Invalid token. Please check your .env file.')
-
+	if not TOKEN:
+		sys.exit('Missing token. Please check your .env file.')
+	else:
+		try:
+			bot.run(TOKEN, log_handler=None)
+		except discord.errors.LoginFailure:
+			sys.exit('Invalid token. Please check your .env file.')
+		except asyncio.CancelledError:
+			if shutdown:
+				pass

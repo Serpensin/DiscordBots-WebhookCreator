67ac6dc826b5c210f6b19eee4a196b4785f28210
Serpensin <wissmann.matti@gmail.com>
2023-11-08 20:21:42 +0100

    owner commands are no app-commands anymore

--- WebhookCreator/main.py
+++ WebhookCreator/main.py
@@ -9,6 +9,7 @@ import platform
 import sentry_sdk
 import sys
 import time
+import traceback
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
@@ -29,13 +30,18 @@ bot_name = 'WebhookCreator'
 if not os.path.exists(app_folder_name):
     os.makedirs(app_folder_name)
 activity_file = os.path.join(app_folder_name, 'activity.json')
-bot_version = "1.3.0"
+bot_version = "1.4.0"
 TOKEN = os.getenv('TOKEN')
 ownerID = os.getenv('OWNER_ID')
 support_id = os.getenv('SUPPORT_SERVER')
 topgg_token = os.getenv('TOPGG_TOKEN')
 heartbeat_url = os.getenv('HEARTBEAT_URL')
 
+# print() will only print if run in debugger. pt() will always print.
+pt = print
+def print(msg):
+    if sys.gettrace() is not None:
+        pt(msg)
 
 #Create activity.json if not exists
 class JSONValidator:
@@ -87,6 +93,7 @@ validator = JSONValidator(activity_file)
 validator.validate_and_fix_json()
 
 
+#Bot
 class aclient(discord.AutoShardedClient):
     def __init__(self):
 
@@ -99,25 +106,142 @@ class aclient(discord.AutoShardedClient):
                         )
         self.synced = False
 
+
+    class Presence():
+        @staticmethod
+        def get_activity() -> discord.Activity:
+            with open(activity_file) as f:
+                data = json.load(f)
+                activity_type = data['activity_type']
+                activity_title = data['activity_title']
+                activity_url = data['activity_url']
+            if activity_type == 'Playing':
+                return discord.Game(name=activity_title)
+            elif activity_type == 'Streaming':
+                return discord.Streaming(name=activity_title, url=activity_url)
+            elif activity_type == 'Listening':
+                return discord.Activity(type=discord.ActivityType.listening, name=activity_title)
+            elif activity_type == 'Watching':
+                return discord.Activity(type=discord.ActivityType.watching, name=activity_title)
+            elif activity_type == 'Competing':
+                return discord.Activity(type=discord.ActivityType.competing, name=activity_title)
+
+        @staticmethod
+        def get_status() -> discord.Status:
+            with open(activity_file) as f:
+                data = json.load(f)
+                status = data['status']
+            if status == 'online':
+                return discord.Status.online
+            elif status == 'idle':
+                return discord.Status.idle
+            elif status == 'dnd':
+                return discord.Status.dnd
+            elif status == 'invisible':
+                return discord.Status.invisible
+
+
+    async def on_message(self, message):
+        async def __wrong_selection():
+            await message.channel.send('```'
+                                       'Commands:\n'
+                                       'help - Shows this message\n'
+                                       'activity - Set the activity of the bot\n'
+                                       'status - Set the status of the bot\n'
+                                       'shutdown - Shutdown the bot\n'
+                                       '```')
+
+        if message.guild is None and message.author.id == int(ownerID):
+            args = message.content.split(' ')
+            print(args)
+            command, *args = args
+            if command == 'help':
+                await __wrong_selection()
+                return
+
+            elif command == 'activity':
+                await Owner.activity(message, args)
+                return
+
+            elif command == 'status':
+                await Owner.status(message, args)
+                return
+
+            elif command == 'shutdown':
+                await Owner.shutdown(message)
+                return
+
+            else:
+                await __wrong_selection()
+
+
     async def on_app_command_error(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError) -> None:
-        await interaction.response.send_message(error, ephemeral = True)
+        options = interaction.data.get("options")
+        option_values = ""
+        if options:
+            for option in options:
+                option_values += f"{option['name']}: {option['value']}"
+        if isinstance(error, discord.app_commands.CommandOnCooldown):
+            await interaction.response.send_message(f'This command is on cooldown.\nTime left: `{str(timedelta(seconds=int(error.retry_after)))}`', ephemeral=True)
+            return
+        if isinstance(error, discord.app_commands.MissingPermissions):
+            await interaction.response.send_message(f'You are missing the following permissions: `{", ".join(error.missing_permissions)}`', ephemeral=True)
+            return
+        else:
+            try:
+                try:
+                    await interaction.response.send_message(f"Error! Try again.", ephemeral=True)
+                except:
+                    try:
+                        await interaction.followup.send(f"Error! Try again.", ephemeral=True)
+                    except:
+                        pass
+            except discord.Forbidden:
+                try:
+                    await interaction.followup.send(f"{error}\n\n{option_values}", ephemeral=True)
+                except discord.NotFound:
+                    try:
+                        await interaction.response.send_message(f"{error}\n\n{option_values}", ephemeral=True)
+                    except discord.NotFound:
+                        pass
+                except Exception as e:
+                    traceback.print_exception(type(error), error, error.__traceback__)
+            finally:
+                traceback.print_exception(type(error), error, error.__traceback__)
+
 
     async def on_ready(self):
         if not self.synced:
+            pt('Syncing commands...')
             await tree.sync()
+            print('Commands synced.')
             self.synced = True
-            await bot.change_presence(activity = discord.Game(name='with Webhooks'), status = discord.Status.online)
-        
+        global owner, start_time, shutdown
+        shutdown = False
+        try:
+            owner = await bot.fetch_user(ownerID)
+            print('Owner found.')
+        except:
+            print('Owner not found.')
+
+        #Start background tasks
         if topgg_token:
             bot.loop.create_task(Functions.topgg())
         if heartbeat_url:
             bot.loop.create_task(Functions.heartbeat())
 
-        global owner, start_time, shutdown
-        shutdown = False
-        owner = await bot.fetch_user(ownerID)
+        await bot.change_presence(activity = self.Presence.get_activity(), status = self.Presence.get_status())
+        pt(r'''
+ __      __          __       __                      __      ____                          __                   
+/\ \  __/\ \        /\ \     /\ \                    /\ \    /\  _`\                       /\ \__                
+\ \ \/\ \ \ \     __\ \ \____\ \ \___     ___     ___\ \ \/'\\ \ \/\_\  _ __    __     __  \ \ ,_\   ___   _ __  
+ \ \ \ \ \ \ \  /'__`\ \ '__`\\ \  _ `\  / __`\  / __`\ \ , < \ \ \/_/_/\`'__\/'__`\ /'__`\ \ \ \/  / __`\/\`'__\
+  \ \ \_/ \_\ \/\  __/\ \ \L\ \\ \ \ \ \/\ \L\ \/\ \L\ \ \ \\`\\ \ \L\ \ \ \//\  __//\ \L\.\_\ \ \_/\ \L\ \ \ \/ 
+   \ `\___x___/\ \____\\ \_,__/ \ \_\ \_\ \____/\ \____/\ \_\ \_\ \____/\ \_\\ \____\ \__/.\_\\ \__\ \____/\ \_\ 
+    '\/__//__/  \/____/ \/___/   \/_/\/_/\/___/  \/___/  \/_/\/_/\/___/  \/_/ \/____/\/__/\/_/ \/__/\/___/  \/_/ 
+        ''')
         start_time = datetime.now()
-        print('READY')
+        pt('READY')
 bot = aclient()
 tree = discord.app_commands.CommandTree(bot)
 tree.on_error = bot.on_app_command_error
@@ -190,23 +314,98 @@ class Functions():
                 pass
 
 
+##Owner Commands
+class Owner():
+    async def activity(message, args):
+        async def __wrong_selection():
+            await message.channel.send('```'
+                                       'activity [playing/streaming/listening/watching/competing] [title] (url) - Set the activity of the bot\n'
+                                       '```')
 
-##Owner Commands----------------------------------------
-#Shutdown
-@tree.command(name = 'shutdown', description = 'Safely shut down the bot.')
-async def self(interaction: discord.Interaction):
-    if interaction.user.id == int(ownerID):
+        if args == []:
+            await __wrong_selection()
+            return
+        action = args[0].lower()
+        title = args[1]
+        try:
+            url = args[2]
+        except IndexError:
+            url = ''
+        print(title)
+        print(url)
+        with open(activity_file, 'r', encoding='utf8') as f:
+            data = json.load(f)
+        if action == 'playing':
+            data['activity_type'] = 'Playing'
+            data['activity_title'] = title
+            data['activity_url'] = ''
+        elif action == 'streaming':
+            data['activity_type'] = 'Streaming'
+            data['activity_title'] = title
+            data['activity_url'] = url
+        elif action == 'listening':
+            data['activity_type'] = 'Listening'
+            data['activity_title'] = title
+            data['activity_url'] = ''
+        elif action == 'watching':
+            data['activity_type'] = 'Watching'
+            data['activity_title'] = title
+            data['activity_url'] = ''
+        elif action == 'competing':
+            data['activity_type'] = 'Competing'
+            data['activity_title'] = title
+            data['activity_url'] = ''
+        else:
+            await __wrong_selection()
+            return
+        with open(activity_file, 'w', encoding='utf8') as f:
+            json.dump(data, f, indent=2)
+        await bot.change_presence(activity = bot.Presence.get_activity(), status = bot.Presence.get_status())
+        await message.channel.send(f'Activity set to {action} {title}{" " + url if url else ""}.')
+
+
+    async def status(message, args):
+        async def __wrong_selection():
+            await message.channel.send('```'
+                                       'status [online/idle/dnd/invisible] - Set the status of the bot\n'
+                                       '```')
+
+        if args == []:
+            await __wrong_selection()
+            return
+        action = args[0].lower()
+        with open(activity_file, 'r', encoding='utf8') as f:
+            data = json.load(f)
+        if action == 'online':
+            data['status'] = 'online'
+        elif action == 'idle':
+            data['status'] = 'idle'
+        elif action == 'dnd':
+            data['status'] = 'dnd'
+        elif action == 'invisible':
+            data['status'] = 'invisible'
+        else:
+            await __wrong_selection()
+            return
+        with open(activity_file, 'w', encoding='utf8') as f:
+            json.dump(data, f, indent=2)
+        await bot.change_presence(activity = bot.Presence.get_activity(), status = bot.Presence.get_status())
+        await message.channel.send(f'Status set to {action}.')
+
+
+    async def shutdown(message):
         global shutdown
+        await message.channel.send('Engine powering down...')
+        await bot.change_presence(status=discord.Status.invisible)
         shutdown = True
-        await interaction.response.send_message('Engine powering down...', ephemeral = True)
 
         tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
         [task.cancel() for task in tasks]
         await asyncio.gather(*tasks, return_exceptions=True)
 
         await bot.close()
-    else:
-        await interaction.response.send_message('Only the BotOwner can use this command!', ephemeral = True)
+        
+
 ##Bot Commands----------------------------------------
 #Bot Information
 @tree.command(name = 'botinfo', description = 'Get information about the bot.')
